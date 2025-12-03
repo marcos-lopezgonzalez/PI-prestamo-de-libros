d35f2b69a30e129ce5b7be8723384a8f270e9fc0
Marcos López González <2005052@alu.murciaeduca.es>
2025-12-03 18:29:50 +0100

    Añado función para procesar login en BBDD

--- app/src/helpers/ayuda.php
+++ app/src/helpers/ayuda.php
@@ -27,3 +27,16 @@ function existeEmail($_email)
     $count = $sentencia->fetchColumn();
     return $count > 0;
 }
+
+function procesarLogin($_username, $_password)
+{
+    $db = new BBDD();
+    $sql = "SELECT COUNT(*) FROM usuario WHERE username = :username and password = :password";
+    $parametros = [
+        "username" => $_username,
+        "password" => password_hash($_password, PASSWORD_DEFAULT)
+    ];
+    $sentencia = $db->getData($sql, $parametros);
+    $count = $sentencia->fetchColumn();
+    return $count > 0;
+}
