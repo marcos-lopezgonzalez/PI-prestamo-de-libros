5e0a4619782bdaf84e6cd47d67aa97add55f6273
Marcos López González <2005052@alu.murciaeduca.es>
2025-12-03 18:42:03 +0100

    Arreglo funcion procesarLogin

--- app/src/helpers/ayuda.php
+++ app/src/helpers/ayuda.php
@@ -31,12 +31,17 @@ function existeEmail($_email)
 function procesarLogin($_username, $_password)
 {
     $db = new BBDD();
-    $sql = "SELECT COUNT(*) FROM usuario WHERE username = :username and password = :password";
+    $sql = "SELECT password FROM usuario WHERE username = :username";
     $parametros = [
-        "username" => $_username,
-        "password" => password_hash($_password, PASSWORD_DEFAULT)
+        "username" => $_username
     ];
+
     $sentencia = $db->getData($sql, $parametros);
-    $count = $sentencia->fetchColumn();
-    return $count > 0;
+    $hash = $sentencia->fetchColumn();
+
+    if ($hash === false) {
+        return false;
+    }
+
+    return password_verify($_password, $hash);
 }
