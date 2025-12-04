a7094ca47851f062ab3a7a4aa5bbc9874818e2b5
Marcos López González <2005052@alu.murciaeduca.es>
2025-12-04 16:43:32 +0100

    Arreglo error script crear-db

--- app/src/config/crear-db.sql
+++ app/src/config/crear-db.sql
@@ -15,7 +15,7 @@ CREATE TABLE usuario (
     apellidos VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
 
 -- -------------------------------------------------
