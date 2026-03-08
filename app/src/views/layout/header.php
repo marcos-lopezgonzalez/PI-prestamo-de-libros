229a845288c3b7fdfcce7f39ce636c20a3618bfe
JuanMiguel <juanmisenpai@gmail.com>
2026-03-08 12:23:24 +0100

    Creo el header | Juan Miguel

---
+++ app/src/views/layout/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="views_css/header.css">
+</head>
+<body>
+    <header class="header">
+    <div class="header__logo">
+        <img src="../../public/img/logo.png" alt="Logo">
+        
+    </div>
+
+    <div class="header__search">
+        <input type="text" placeholder="Buscar libros...">
+        <button>🔍</button>
+    </div>
+
+    <div class="header__user">
+        <span>Hola,Usuario</span>
+    </div>
+</header>
+</body>
+</html>
